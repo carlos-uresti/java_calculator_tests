9e51e34c2872ad7c224a6b9e8e109b93ac2c937e
carlos-uresti <80980080+carlos-uresti@users.noreply.github.com>
2022-10-02 11:21:41 -0500

    Update ArithmeticOperationsTest.java

--- ArithmeticOperationsTest.java
+++ ArithmeticOperationsTest.java
@@ -11,23 +11,23 @@ public class ArithmeticOperationsTest {
     String[] string1 = {"5.0", "4.0", "+"};
 
 
-    //Tests for method result()
+    //Tests for method results()
     @Test
-    void additionCorrectTest()
+    void additionCorrectTest1()
     {
         assertEquals("2.0", arithmeticOperations.result(new String[]{"1", "1", "+"},3));
 
     }
 
     @Test
-    void additionIncorrectTest()
+    void additionIncorrectTest2()
     {
         assertNotEquals("2.0", arithmeticOperations.result(new String[]{"1", "1", "*"},3));
 
     }
 
     @Test
-    void subtractionCorrectTest()
+    void subtractionCorrectTest3()
     {
 
         assertEquals("0.0", arithmeticOperations.result(new String[]{"1", "1", "-"},3));
@@ -35,7 +35,7 @@ public class ArithmeticOperationsTest {
     }
 
     @Test
-    void subtractionIncorrectTest()
+    void subtractionIncorrectTest4()
     {
 
         assertNotEquals("0.0", arithmeticOperations.result(new String[]{"1", "1", "*"},3));
@@ -44,112 +44,112 @@ public class ArithmeticOperationsTest {
 
 
     @Test
-    void multiplicationCorrectTest()
+    void multiplicationCorrectTest5()
     {
         assertEquals("4.0", arithmeticOperations.result(new String[]{"2", "2", "*"},3));
 
     }
 
     @Test
-    void multiplicationIncorrectTest()
+    void multiplicationIncorrectTest6()
     {
         assertNotEquals("4.0", arithmeticOperations.result(new String[]{"2", "2", "÷"},3));
 
     }
 
     @Test
-    void divisionCorrectTest()
+    void divisionCorrectTest7()
     {
         assertEquals("1.0", arithmeticOperations.result(new String[]{"2", "2", "÷"},3));
 
     }
 
     @Test
-    void divisionIncorrectTest()
+    void divisionIncorrectTest8()
     {
         assertNotEquals("1.0", arithmeticOperations.result(new String[]{"2", "2", "*"},3));
 
     }
 
     @Test
-    void postfixArrayEmpty()
+    void postfixArrayEmptyTest9()
     {
         assertEquals("", arithmeticOperations.result(new String[]{},0));
 
     }
 
     @Test
-    void postfixArrayNotEmpty()
+    void postfixArrayNotEmptyTest10()
     {
         assertNotEquals("", arithmeticOperations.result(new String[]{"0"},1));
 
     }
 
     @Test
-    void postfixArraySizeZero()
+    void postfixArraySizeZeroTest11()
     {
         assertEquals("", arithmeticOperations.result(new String[]{},0));
 
     }
 
     @Test
-    void postfixArraySizeOne()
+    void postfixArraySizeOneTest12()
     {
         assertEquals("1", arithmeticOperations.result(new String[]{"1"},1));
 
     }
 
     @Test
-    void postfixArraySizeGreaterThanOne()
+    void postfixArraySizeGreaterThanOneTest13()
     {
         assertEquals("2.0", arithmeticOperations.result(new String[]{"1", "1", "+"},3));
 
     }
 
     @Test
-    void isPostfixArrayCountIntType()
+    void isPostfixArrayCountIntTypeTest14()
     {
         assertEquals("4.0", arithmeticOperations.result(new String[]{"2", "2", "*"},3));
     }
 
     @Test
-    void isPostfixArrayCountNotIntType()
+    void isPostfixArrayCountNotIntTypeTest15()
     {
         assertNotEquals("", arithmeticOperations.result(new String[]{"2", "2", "*"}, Integer.parseInt("3")));
     }
 
     @Test
-    void isPostFixArrayStringType()
+    void isPostFixArrayStringTypeTest16()
     {
         assertEquals("4.0", arithmeticOperations.result(new String[]{"2", "2", "*"},3));
     }
 
     @Test
-    void isPostFixArrayNotStringType()
+    void isPostFixArrayNotStringTypeTest17()
     {
         assertNotEquals("", arithmeticOperations.result(new String[]{String.valueOf(2), String.valueOf(2), "+"},3));
     }
 
     @Test
-    void postfixArrayCountEqualNull()
+    void postfixArrayCountEqualNullTest18()
     {
         assertEquals("", arithmeticOperations.result(new String[]{}, null));
     }
 
     @Test
-    void postfixArrayCountNotEqualNull()
+    void postfixArrayCountNotEqualNullTest19()
     {
         assertNotEquals("", arithmeticOperations.result(new String[]{"2", "2", "*"}, 3));
     }
 
     @Test
-    void postfixArrayEqualNull()
+    void postfixArrayEqualNullTest20()
     {
         assertEquals("", arithmeticOperations.result(null, 0));
     }
 
     @Test
-    void postfixArrayNotEqualNull()
+    void postfixArrayNotEqualNullTest21()
     {
         assertNotEquals("", arithmeticOperations.result(new String[]{"2", "2", "*"}, 3));
     }
